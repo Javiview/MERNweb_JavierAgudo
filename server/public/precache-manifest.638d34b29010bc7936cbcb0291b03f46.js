4a50d3092e6184cbba07aef670d95c918737aacd
Javier Agudo <javieragudo3d@gmail.com>
2019-12-17 10:31:26 +0100

    Heroku

---
+++ server/public/precache-manifest.638d34b29010bc7936cbcb0291b03f46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bb0c1b2f15c0a703f14a079c0bc6670",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66fcebb7ffb498472c67",
+    "url": "/static/css/main.08a4b962.chunk.css"
+  },
+  {
+    "revision": "b0c99a0ed0b2b5864ef4",
+    "url": "/static/js/2.cb8d26ad.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/static/js/2.cb8d26ad.chunk.js.LICENSE"
+  },
+  {
+    "revision": "66fcebb7ffb498472c67",
+    "url": "/static/js/main.7629559e.chunk.js"
+  },
+  {
+    "revision": "2bcb8084869f23f72706",
+    "url": "/static/js/runtime-main.88d4f3f8.js"
+  }
+]);
